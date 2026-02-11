220abc01c40f395c325a71321f4e96fb94c7650d
Seunghwan Hwang <63487622+shwan6160@users.noreply.github.com>
2026-02-11 13:52:11 +0000

    GoogleGenai 클래스 다듬기

--- provider.py
+++ provider.py
@@ -1,30 +1,34 @@
-from typing import Optional
+from typing import Optional, Any, Protocol
+from dataclasses import dataclass
 from abc import ABC, abstractmethod
 
 from google import genai
 
+
+class ModelConfigLike(Protocol):
+    temperature: float | None
+    top_p: float | None
+    top_k: int | float | None
+
+
+@dataclass
 class ModelConfig:
-    system_prompt: Optional[str] = None
     temperature: float
     top_p: float
     top_k: int
-    api_key: Optional[str] = None
+    system_prompt: Optional[str] = None
 
 
 class ModelProvider(ABC):
-    def __init__(self, config: ModelConfig):
+    def __init__(self, config: ModelConfigLike):
         self.config = config
     
     @abstractmethod
     def generate_content(self, user_prompt: str) -> str:
         """
-        실제 텍스트 생성 로직. 
+        Text Generation Function. 
         """
         return str()
-
-    @property
-    def api_key(self) -> Optional[str]:
-        return self.config.api_key
 
 
 class GoogleGenai(ModelProvider):
@@ -37,35 +41,24 @@ class GoogleGenai(ModelProvider):
             models.append(m.name)
         return models
 
-
-    def __init__(self, config: ModelConfig, model_name: str = "gemini-3-flash"):
+    def __init__(self, config: genai.types.GenerateContentConfig, api_key: str, model_name: str = "gemini-3-flash"):
         super().__init__(config)
 
-        if not self.config.api_key:
+        if not api_key:
             raise ValueError("API 키가 설정되지 않았습니다.")
 
+        self.api_key = api_key
         self.model_name = model_name
-        
     
     def generate_content(self, user_prompt: str) -> str:
-        prompt = ""
-
-        if not self.config.system_prompt:
-            prompt = user_prompt
-        else:
-            prompt = f"{self.config.system_prompt}\n\n{user_prompt}\n"
-
-        client = genai.Client(api_key=self.config.api_key)
+        client = genai.Client(api_key=self.api_key)
 
         response = client.models.generate_content(
             model = self.model_name,
-            contents = genai.types.Part.from_text(text = prompt),
-            config = genai.types.GenerateContentConfig(
-                temperature = self.config.temperature,
-                top_p = self.config.top_p,
-                top_k = self.config.top_k
+            contents = genai.types.Part.from_text(text = user_prompt),
+            config = self.config
             )
-        )
+
         if not response or not response.text:
             raise ValueError("모델로부터 유효한 응답을 받지 못했습니다.")
 
