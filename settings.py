15015ba704e40c5c8a25d3bb3b76788877e91ff1
Seunghwan Hwang <63487622+shwan6160@users.noreply.github.com>
2026-02-15 05:36:52 +0000

    파일 이름 오타

---
+++ settings.py
@@ -0,0 +1,25 @@
+EPUB = {
+    "font": "RIDIBatang.otf",
+    "style": "style.css"
+}
+
+GEMINI = {
+    "temperature": 0.9,
+    "top_p": 0.9,
+    "top_k": 40,
+    "text_block_size": 35000
+}
+
+GEMMA = {
+    "temperature": 1.0,
+    "top_p": 0.95,
+    "top_k": 64,
+    "text_block_size": 10000
+}
+
+ROSETTA = {
+    "temperature": 1.0,
+    "top_p": 0.95,
+    "top_k": 64,
+    "text_block_size": 10000
+}
