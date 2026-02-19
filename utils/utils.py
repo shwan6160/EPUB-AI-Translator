e7abbab2e327ca0dcfa0fa60b7d994e35dae9d0d
Seunghwan Hwang <63487622+shwan6160@users.noreply.github.com>
2026-02-19 13:55:16 +0000

    utils 재구조화 2

--- utils/utils.py
+++ utils/utils.py
@@ -7,48 +7,6 @@ from PIL import Image
 
 import settings
 
-# cli utils
-def select_provider(provider_select: str|None = None) -> str:
-    provider_list = ["Google", "OpenRouter", "Copilot"]
-    if provider_select in provider_list:
-        return provider_select
-
-    print("사용할 모델 제공자를 선택하세요:")
-    for p in enumerate(provider_list, start=1):
-        print(f"{p[0]}. {p[1]}")
-
-    while True:
-        provider_select = input("Provider: ")
-        if provider_select.isdigit() and 1 <= int(provider_select) <= len(provider_list):
-            return provider_list[int(provider_select)-1]
-        elif provider_select in provider_list:
-            return provider_select
-        else:
-            print("잘못된 입력입니다. 다시 입력하십시오.")
-
-def select_model(available_models: list[str]) -> str:
-    print("사용 가능한 모델 목록:")
-    for m in enumerate(available_models, start=1):
-        print(f"{m[0]}. {m[1]}")
-    while True:
-        model_select = input("모델: ")
-        if model_select.isdigit() and 1 <= int(model_select) <= len(available_models):
-            return available_models[int(model_select)-1]
-        else:
-            print("잘못된 입력입니다. 다시 입력하십시오.")
-
-def yn_check(yes: bool, prompt: str) -> bool:
-    if yes:
-        return True
-    while True:
-        user_input = input(prompt + " (y/n):").strip().lower()
-        if user_input in ['y', 'yes']:
-            return True
-        elif user_input in ['n', 'no']:
-            return False
-        else:
-            print("'y' 또는 'n'을 입력하십시오.")
-
 # 함수 utils
 def get_workspace() -> Path:
     workspace = Path.home() / ".epub_ai_translator"
